83999cd9ed2fc7b85f4f66b2ef86ab568dc3fc90
Apxupok <apxupok@gmail.com>
2022-08-25 06:03:37 +0500

    Added button

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -1,29 +1,28 @@
 package com.example.composequadrant
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+import com.example.composequadrant.ui.theme.Shapes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,10 +71,24 @@ fun Screen() {
             )
         }
     }
+    ExtendedFloatingActionButton(
+        onClick = { /* ... */ },
+        contentColor = Color.Red,
 
+                icon = {
+            Icon(
+                Icons.Filled.Favorite,
+                contentDescription = "Favorite",
+                modifier = Modifier.size(50.dp)
+
+            )
+        },
+        text = { Text("Like", modifier = Modifier.size(50.dp)) }
+    )
 }
 
-@Composable
+
+    @Composable
 fun take_text(art:String,
               descripion:String,
               modifier: Modifier = Modifier,
